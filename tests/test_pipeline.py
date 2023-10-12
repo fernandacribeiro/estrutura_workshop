2a1cf84f93dfe2ba39fdfb5458c403930961fabd
fernandacribeiro <fernandafcrk@gmail.com>
2023-10-12 20:15:26 -0300

    Arquivo para transformação dos dados e arquivo para teste do método de transformação

---
+++ tests/test_pipeline.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from src.app.pipeline.transform import concat_data_frames
+
+df_1 = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+df_2 = pd.DataFrame({'col1': [5, 6], 'col2': [7, 8]})
+
+def testar_concatenacao_lista_dataframes():
+
+    # arrange
+    data_frame_list = [df_1, df_2]
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
+
+    #act
+    df = concat_data_frames(data_frame_list)
+
+    assert df.shape == (4, 2)
+    assert data_frame.equals(df)
+    assert df.shape != (5, 2)
